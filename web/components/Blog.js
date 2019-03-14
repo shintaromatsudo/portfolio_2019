8db91f0319d5242a11a24813279acbf64d69666a
ShintaroMatsudo <matsudo.sh@cnt.ncos.nec.co.jp>
2019-03-14 17:32:28 +0900

    routing

--- web/components/Blog.js
+++ web/components/Blog.js
@@ -1,25 +1,27 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import PropTypes from 'prop-types'
-import { Router } from '../routes'
+import { Link, Router } from '../routes'
 import { detailData } from '../actions'
 
 const Blog = props => {
   const { id, title, content } = props
   console.log(props)
 
-  const onClick = e => {
-    e.preventDefault()
-    // props.detailData(id)
-    console.log(id)
-    Router.pushRoute(`/blog/${id}`)
-  }
+//  const onClick = e => {
+//    e.preventDefault()
+//    props.detailData(id)
+//    console.log(id)
+//    Router.pushRoute(`/blog/${id}`)
+//  }
 
   return (
     <div>
       <h2>{title}</h2>
       <p>{content}</p>
-      <button onClick={onClick}>MORE</button>
+//      <button onClick={onClick}>MORE</button>
+      <Link as={`/${id`} href={`detail?title=${title}&content=${content}`}><a>MORE</a></Link>
+//      <Link href={{ pathname:'blog/detail', query:{${title}, ${content}} as='blog/${id}'><a>MORE</a></Link>
     </div>
   )
 }
