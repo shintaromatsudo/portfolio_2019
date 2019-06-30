4c98f97b459988bca90e2befe61bc6b668ac3246
shintaro matsudo <shintaro011224@gmail.com>
2019-06-30 13:33:06 +0900

    preparation

---
+++ web/components/Header.js
@@ -0,0 +1,12 @@
+import Button from './Button'
+
+const Header = () => (
+  <div>
+    <p>Hello Misa,Sara!</p>
+    <Button href="about" name="About" />
+    <Button href="blog" name="Blog" />
+    <Button href="works" name="Works" />
+  </div>
+)
+
+export default Header
